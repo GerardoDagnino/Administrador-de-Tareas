bfae956e4cf45cdea451a8fa7816081234c28cc4
Gerardo Dagnino <gerardoangecala@gmail.com>
2023-12-30 21:59:50 -0500

    avanzandi

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -22,33 +21,23 @@ def load_user(user_id):
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    start_date = db.Column(db.DateTime, nullable=False)
+    end_date = db.Column(db.DateTime)
 
-@app.route('/')
+@app.route('/admin/tasks')
 @login_required
-def index():
+def get_tasks():
     tasks = Task.query.all()
-    return render_template('index.html', tasks=tasks)
+    events = []
+    for task in tasks:
+        events.append({
+            'title': task.title,
+            'start': task.start_date.isoformat(),
+            'end': task.end_date.isoformat() if task.end_date else None
+        })
+    return jsonify({'tasks': events})
 
-@app.route('/add', methods=['POST'])
-@login_required
-def add():
-    title = request.form['title']
-    new_task = Task(title=title)
-    db.session.add(new_task)
-    db.session.commit()
-    return redirect(url_for('index'))
-
-# ...
-
-@app.route('/events', methods=['GET'])
-@login_required
-def get_events():
-    tasks = Task.query.all()
-    events = [{'id': task.id, 'title': task.title, 'start': task.created_at.isoformat()} for task in tasks]
-    return jsonify(events)
-
-# ...
+# Resto del código (mantén las rutas existentes)
 
 if __name__ == '__main__':
     with app.app_context():
