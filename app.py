185e75bca4ec27dcd3c3b6df37419f5c9e9c2c75
Gerardo Dagnino <gerardoangecala@gmail.com>
2024-01-04 00:08:06 -0500

    pruebo3

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
+    tasks = db.relationship('Task', backref='user', lazy=True)
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -26,20 +27,14 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
     start_date = db.Column(db.String(20), nullable=False)
-
-# Ruta a la página principal
-@app.route('/')
-@login_required
-def index():
-    tasks = Task.query.all()
-    return render_template('index.html', tasks=tasks)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 # Ruta para agregar tarea
 @app.route('/add', methods=['POST'])
 @login_required
 def add():
     title = request.form['title']
-    new_task = Task(title=title)
+    new_task = Task(title=title, user=current_user)
     db.session.add(new_task)
     db.session.commit()
 
@@ -49,51 +44,11 @@ def add():
 
     return redirect(url_for('index'))
 
-# Ruta para la página de inicio de sesión
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        user = User.query.filter_by(username=username).first()
-        if user and user.password == password:
-            login_user(user)
-            flash('Login successful!', 'success')
-            return redirect(url_for('admin'))
-        else:
-            flash('Invalid username or password', 'error')
-    return render_template('login.html')
-
-# Ruta para la página de registro
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        existing_user = User.query.filter_by(username=username).first()
-        if existing_user:
-            flash('Username already exists. Please choose a different one.', 'error')
-        else:
-            new_user = User(username=username, password=password)
-            db.session.add(new_user)
-            db.session.commit()
-            flash('Registration successful! Please log in.', 'success')
-            return redirect(url_for('login'))
-    return render_template('register.html')
-
-# Ruta para cerrar sesión
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    flash('Logout successful!', 'success')
-    return redirect(url_for('login'))
-
 # Ruta para la página de administración
 @app.route('/admin')
 @login_required
 def admin():
-    tasks = Task.query.all()
+    tasks = Task.query.filter_by(user=current_user).all()
     return render_template('admin.html', tasks=tasks)
 
 # Ruta para agregar tarea desde la página de administración
@@ -101,7 +56,7 @@ def admin():
 @login_required
 def add_task_admin():
     title = request.form['title']
-    new_task = Task(title=title)
+    new_task = Task(title=title, user=current_user)
     db.session.add(new_task)
     db.session.commit()
 
@@ -110,6 +65,8 @@ def add_task_admin():
         file.write(f"{title}\n")
 
     return redirect(url_for('admin'))
+
+
 @app.route('/admin/delete/<int:task_id>', methods=['GET'])
 @login_required
 def delete_task_admin(task_id):
@@ -147,5 +104,5 @@ if __name__ == '__main__':
     if not os.path.exists('instance'):
         os.makedirs('instance')
 
-        # Iniciar la aplicación
+    # Iniciar la aplicación
     app.run(debug=True)
